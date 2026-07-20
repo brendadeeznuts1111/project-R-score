fc9f8506b9cd940b457b0abec8514f0541e31b2a
nolarose <nolarose@factory-wager.com>
2026-07-20 09:22:42 -0500

    style(lib): format repo-docs after harness external refs

--- lib/docs/repo-docs.ts
+++ lib/docs/repo-docs.ts
@@ -104,8 +104,7 @@ export const CANONICAL_EXTERNAL = {
       'https://github.com/lopopolo/harness-engineering/blob/trunk/docs/domain-modeling/README.md',
     durableSystems:
       'https://github.com/lopopolo/harness-engineering/blob/trunk/docs/durable-systems/README.md',
-    proof:
-      'https://github.com/lopopolo/harness-engineering/blob/trunk/docs/proof/README.md',
+    proof: 'https://github.com/lopopolo/harness-engineering/blob/trunk/docs/proof/README.md',
     /** Prefer "artifact" over "codebase" in agent/docs prose */
     replaceCodebaseWithArtifact: 'https://x.com/_lopopolo/status/2076878736507736390',
     codeIsNotTheArtifact: 'https://hyperbo.la/w/code-is-not-the-artifact/',
